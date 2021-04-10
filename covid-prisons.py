90a2c595181caa1d9a55c64cd2a5d009a8035da0
Anne Li <24998524+fibanneacci@users.noreply.github.com>
2021-04-10 11:10:54 -0500

    update headers

--- covid-prisons.py
+++ covid-prisons.py
@@ -23,8 +23,9 @@ COVID_PRISON_DATA_URL = ('https://raw.githubusercontent.com/themarshallproject/C
 def load_covid_prison_data():
     covid_prison_data = pd.read_csv(COVID_PRISON_DATA_URL, nrows = 50,
                                     names = ['name', 'abbreviation', 'staff_tests', 'staff_tests_with_multiples', 'total_staff_cases',
-                                             'staff_recovered', 'total_staff_deaths', 'prisoner_tests', 'prisoner_test_with_multiples',
-                                             'total_prisoner_cases', 'prisoners_recovered', 'total_prisoner_deaths', 'as_of_date', 'notes'],
+                                             'staff_recovered', 'total_staff_deaths', 'staff_partial_dose', 'staff_full_dose',
+                                             'prisoner_tests', 'prisoner_test_with_multiples', 'total_prisoner_cases', 'prisoners_recovered',
+                                             'total_prisoner_deaths', 'prisoners_partial_dose', 'prisoners_full_dose', as_of_date', 'notes'],
                                     usecols = ['name', 'total_prisoner_cases', 'total_prisoner_deaths', 'as_of_date'],
                                     skiprows = 103, # Change according to date
                                     )
